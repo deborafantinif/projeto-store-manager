195863ee353520363c078bc78b41fc26fc0d2bd0
deborafantinif <deboraferfan@gmail.com>
2022-07-04 16:02:05 -0300

    feat: requisito 01

---
+++ models/productModel.js
@@ -0,0 +1,17 @@
+const connection = require('./connection');
+
+const productModel = {
+  getAll: async () => {
+    const sql = 'SELECT * FROM StoreManager.products';
+    const [products] = await connection.query(sql);
+    return products;
+  },
+
+  getById: async (id) => {
+    const sql = 'SELECT * FROM StoreManager.products WHERE id = ?';
+    const [[product]] = await connection.query(sql, id);
+    return product;
+  },
+};
+
+module.exports = productModel;
---
+++ services/productService.js
@@ -0,0 +1,16 @@
+const productModel = require('../models/productModel');
+
+const productService = {
+  getAll: async () => {
+    const data = await productModel.getAll();
+    return data;
+  },
+
+  getById: async (id) => {
+    const data = await productModel.getById(id);
+    if (!data) return { code: 404, data: { message: 'Product not found' } };
+    return { code: 200, data };
+  },
+};
+
+module.exports = productService;
